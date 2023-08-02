3fa1911f0fe0cf5a70c1e3289b87877b5ae42a82
LamPham <lam.pham@whl.travel>
2023-08-02 13:21:21 +0700

    #init project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-import './style.css'
+import './layout.scss'
+import './style.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,70 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
+module.exports = (env, argv) => {
+    return {
+        entry: {
+            main: './src/index.js',
         },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                include: path.resolve(__dirname, 'src'),
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js'
+        },
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, 'dist'),
             },
+            port: 3000,
+            open: true,
+            hot: true,
+            compress: true,
+            historyApiFallback: true,
+        },
+        optimization: {
+            minimizer: [
+                new CssMinimizerPlugin(),
+            ],
+            minimize: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    include: path.resolve(__dirname, 'src'),
+                    use: [
+                        'style-loader',
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                esModule: false,
+                            },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        'postcss-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin()
         ],
-    },
-    mode: 'development'
+        mode: 'development'
+    }
 }
